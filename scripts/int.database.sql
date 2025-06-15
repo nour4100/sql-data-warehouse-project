878a1a167c117d34cb93c43b0c4834aa4bdf063f
nour4100 <nwrm21852@gmail.com>
2025-06-15 16:07:52 +0300

    Create int.database.sql

---
+++ scripts/int.database.sql
@@ -0,0 +1,18 @@
+USE master;
+GO
+
+--Create the 'Datawarehouse' database
+CREATE DATABASE Datawearhouse;
+GO
+
+USE Datawearhouse;
+GO
+
+--Create schema
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
