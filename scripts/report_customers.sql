c1d23addca21d302c0b01adac09d2c159877eb49
Nour <nwrm21852@gmail.com>
2025-06-30 11:32:26 +0300

    Update report_customers.sql

--- scripts/report_customers.sql
+++ scripts/report_customers.sql
@@ -1,24 +1,24 @@
 /*
 ===============================================================================
-Product Report
+Customer Report
 ===============================================================================
 Highlights:
-    1. Gathers essential fields such as product name, category, subcategory, and cost.
-    2. Segments products by revenue to identify High-Performers, Mid-Range, or Low-Performers.
-    3. Aggregates product-level metrics:
-       - total orders
-       - total sales
-       - total quantity sold
-       - total customers (unique)
-       - lifespan (in months)
+    1. Gathers essential fields such as names, ages, and transaction details.
+    2. Segments customers into categories (VIP, Regular, New) and age groups.
+    3. Aggregates customer-level metrics:
+	   - total orders
+	   - total sales
+	   - total quantity purchased
+	   - total products
+	   - lifespan (in months)
     4. Calculates valuable KPIs:
-       - recency (months since last sale)
-       - average order revenue (AOR)
-       - average monthly revenue
-===============================================================================
+	        - recency (months since last order)
+		- average order value
+		- average monthly spend
+================================================================================
 */
 -- =============================================================================
--- Create Report: gold.report_products
+-- Create Report: gold.report_customers
 -- =============================================================================
 CREATE VIEW gold.report_products AS
 /*---------------------------------------------------------------------------
